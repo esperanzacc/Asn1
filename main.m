ca817a0e1d6888b4d0845f5a88af306bf728a8e6
esperanzacc <v.esperanzac113@gmail.com>
2022-05-10 10:02:43 -0500

    Add 9operations

--- main.m
+++ main.m
@@ -6,51 +6,109 @@
 //
 
 #import <Foundation/Foundation.h>
+
 NSString *getUserInput(NSString *prompt) {
   char inputChars[255];
   
   NSLog(@"%@", prompt);
   fgets(inputChars, 255, stdin);
+  
   NSString *str = [NSString stringWithUTF8String:inputChars];
   return [str stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
 }
+
+NSInteger getNumInput(NSString *num) {
+  NSString *optionsList = @"\nChoose one of the following options:\n1. Uppercase \n2. Lowercase \n3. Numberize \n4. Canadianize \n5. Respond \n6. De-Space-It \n7. Word Count \n8. Remove Punctuations \n9. Done";
+  
+  while (true) {
+    NSLog(@"%@",num);
+    NSLog(@"%@",optionsList);
+    
+    char inputChars[255];
+    fgets(inputChars, 255, stdin);
+    
+    NSString *numInput = [NSString stringWithUTF8String:inputChars];
+    NSInteger intNumInput = [numInput integerValue];
+    
+    if (intNumInput >= /* DISABLES CODE */ (1) || intNumInput <= 9) {
+      return intNumInput;
+    } else {
+      NSLog(@"Plese input num from 1 to 9.");
+    }
+  }
+}
+
 
 int main(int argc, const char * argv[]) {
   @autoreleasepool {
     NSString *input = getUserInput(@"\nEnter your String: ('q' to quit)");
   
-    NSString *optionsList = @"\nChoose one of the following options:\n1. Uppercase \n2. Lowercase \n3. Numberize \n4. Canadianize \n5. Respond \n6. De-Space-It \n7. Word Count \n8. Remove Punctuations \n9. Done";
-    
     if (![input isEqualToString:@"q"]) {
-      NSLog(@"%@", optionsList);
-      NSString *numInput = getUserInput(@"\n");
-      if ([numInput isEqualToString:@"1"]) {
-        NSLog(@"%@", [input uppercaseString]);
-      } else if ([numInput isEqualToString:@"2"]) {
-        NSLog(@"%@", [input lowercaseString]);
-      } else if ([numInput isEqualToString:@"3"]) {
-        if ([input intValue]){
-          NSLog(@"%d", [input intValue]);
-        } else {
-          NSLog(@"Input Cannot Be Converted to Integer.");
+      NSMutableString *s = [NSMutableString stringWithString:input];
+      
+      BOOL loopList = YES;
+      while (loopList) {
+        NSInteger choiceInput = getNumInput(@"");
+        
+        // different options
+        switch (choiceInput) {
+          case 1:
+            // Uppercase
+            NSLog(@"%@", [s uppercaseString]);
+            break;
+          case 2:
+            // Lowercase
+            NSLog(@"%@", [s lowercaseString]);
+            break;
+          case 3:
+            // Numberize; if can not do it -> Input Cannot Be Converted to Integer.
+            if ((NSInteger *)[s integerValue]) {
+              NSLog(@"%ld", (long)[s integerValue]);
+            } else {
+              NSLog(@"Input Cannot Be Converted to Integer.");
+            }
+            break;
+          case 4:
+            // Canadianize + ,eh?
+            [s appendString:@", eh?"];
+            NSLog(@"%@", s);
+            break;
+          case 5:
+            // Respond, if end with ? -> "I don't know". if end with ! -> "Whoa, calm down!".
+          {
+            NSString *lastChar = [s substringFromIndex: s.length - 1];
+            if ([lastChar isEqualToString:@"?"]) {
+              NSLog(@"I don't know.");
+            } else if ([s isEqualToString:@"!"]) {
+              NSLog(@"Whoa, calm down!");
+            }
+            break;
+          }
+          case 6:
+            // De-Space-It: Replace all spaces with "-".
+          {
+            NSString *replaceSpaces = [s stringByReplacingOccurrencesOfString:@" " withString:@"-"];
+            NSLog(@"%@", replaceSpaces);
+            break;
+          }
+          case 7:
+            // Word Count
+            NSLog(@"%lu", (unsigned long)[[s componentsSeparatedByString: @" "] count]);
+            break;
+          case 8:
+            // Remove Punctuations
+          {
+            NSString* finish = [[s componentsSeparatedByCharactersInSet:[[NSCharacterSet letterCharacterSet] invertedSet]] componentsJoinedByString:@""];
+            
+            NSLog(@"%@", finish);
+            break;
+          }
+          case 9:
+            loopList = NO;
+            break;
         }
-      } else if ([numInput isEqualToString:@"4"]) {
-        NSMutableString *s = [NSMutableString stringWithString:input];
-        [s appendString:@", eh?"];
-        NSLog(@"%@", s);
-      } else if ([numInput isEqualToString:@"5"]) {
-        NSString *respond = [input substringFromIndex: [input length] - 1];
-        if ([respond isEqualToString:@"?"]) {
-          NSLog(@"I don't know.");
-        } else if ([respond isEqualToString:@"!"]) {
-          NSLog(@"Whoa, calm down!");
-        }
-      } else if ([numInput isEqualToString:@"6"]) {
-        NSString *replaceString = [input stringByReplacingOccurrencesOfString:@" " withString:@"-"];
-        NSLog(@"%@", replaceString);
-      }
-    
-    }
-  }
+       }
+     }
+   }
   return 0;
 }
